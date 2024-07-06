2e5729cb4cc2f375e8e49c2ee23bd2a4acf9503e
Lost-in-Noise <nancykikvadze@gmail.com>
2024-07-06 18:10:09 +0400

    Add Header and Footer components with styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 TheXPlace.ai. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
